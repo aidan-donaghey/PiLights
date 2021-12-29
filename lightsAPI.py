c1bfdd8987eb8a2dd4826654d0ca17afa01bffd8
Aidan Donaghey <aidan.donaghey@yale.edu>
2021-12-29 17:53:56 -0500

    change

--- lightsAPI.py
+++ lightsAPI.py
@@ -1,9 +1,20 @@
 import json
-import lightsController as lc
+import time
+# import lightsController as lc
+from rpi_ws281x import *
 sP = "/rasplights/"
 
 class lights():
   def __init__(self):
+    # LED strip configuration:
+    LED_COUNT      = 300      # Number of LED pixels.
+    LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+    #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+    LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+    LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+    LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+    LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+    LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
     self.actions = {
         sP + "on": self.lightsOn,
         sP + "off": self.lightsOff,
@@ -14,6 +25,7 @@ class lights():
         sP + "setcolor": self.setcolor,
 
     }
+    self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     self.brightness = 100
     self.color = (255,255,255)
   
@@ -37,8 +49,17 @@ class lights():
     color = [x["color"]["r"],x["color"]["g"],x["color"]["b"]]
     self.color = color
     print(f"Lights changed color to {*color,}")
-    lc.solidColor(color)
+    self.solidColor(color)
 
+  def solidColor(self,color):
+  # Converts the color from the json to the rpi_ws281x color
+    finalcolor = Color(255, 0, 0)
+    t1 = time.time()
+    for i in range(self.strip.numPixels()):
+        self.strip.setPixelColor(i, finalcolor)
+    t2 = time.time()
+    print(t2-t1)
+    self.strip.show()
 
   def lightsBlue(self,*argv):
     print("Lights Blue")
