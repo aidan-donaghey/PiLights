cfd6bd48de91dfcafe7e60c923ba197cda54f1e5
Aidan Donaghey <aidan.donaghey@yale.edu>
2021-12-29 14:23:22 -0500

    Updating the MQTTpi.py should be able to run on pi and accepty all /rasplights inputs

--- MQTTclient.py
+++ MQTTclient.py
@@ -2,22 +2,41 @@ import time
 import paho.mqtt.client as paho
 broker="192.168.1.127"
 #define callback
+
+def connect_mqtt() -> paho:
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    client = paho.Client("client-001")
+    client.on_connect = on_connect
+    client.connect(broker)
+    return client
+
+
+
 def on_message(client, userdata, message):
     time.sleep(1)
     print("received message =",str(message.payload.decode("utf-8")))
 
 client= paho.Client("client-001") #create client object client1.on_publish = on_publish #assign function to callback client1.connect(broker,port) #establish connection client1.publish("house/bulb1","on")
-######Bind function to callback
-client.on_message=on_message
-#####
-print("connecting to broker ",broker)
-client.connect(broker)#connect
-client.loop_start() #start loop to process received messages
-print("subscribing")
-# client.subscribe("house/bulb1")#subscribe
-time.sleep(2)
-print("publishing ")
-client.publish("test","on")#publish
-time.sleep(4)
-client.disconnect() #disconnect
-client.loop_stop() #stop loop
+
+topic = "test"
+
+def subscribe(client: paho):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+
+def run():
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
+
+
+if __name__ == '__main__':
+    run()
---
+++ MQTTpi.py
@@ -0,0 +1,57 @@
+import paho.mqtt.client as paho
+import time
+# Broker and User information
+broker="192.168.1.127"
+# This is the topic that we are subscribing to
+#  It should be subbed to all topics inside of rasplights with the #
+topic = "/rasplights/#"
+
+
+def connect_mqtt() -> paho:
+    """This connects to the Broker
+
+    Returns:
+        paho: This is the cleint Object
+    """
+    def on_connect(client, userdata, flags, rc):
+        """Callback function for connection to the broker
+
+        Args:
+            client (paho): Client Object
+            userdata (dict): Optional
+            flags ([type]): [description]
+            rc ([type]): [description]
+
+        Returns:
+            [paho]: Returns the client after it has been connected or not.
+        """
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    client = paho.Client("client-001")
+    # Binds the on_connect function above to the client.
+    client.on_connect = on_connect
+    # Actually connects and calls on_connect
+    client.connect(broker)
+    return client
+
+
+def subscribe(client: paho):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+
+def run():
+    """Main Running Loop
+    """
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
+
+
+if __name__ == '__main__':
+    run()
